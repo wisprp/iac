477331c55e10d37bb7c554df87d4585cea469044
Ivan Smalzer <wisprp@gmail.com>
2020-05-31 20:57:25 +0200

    add default sg with 22/80/443 ports for standalone

--- aws/stand-alone/instance/main.tf
+++ aws/stand-alone/instance/main.tf
@@ -23,13 +23,14 @@ data "aws_ami" "ubuntu_lts" {
 }
 
 resource "aws_instance" "stal_instance" {
-  ami           = data.aws_ami.ubuntu_lts.id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.stal_subnet.id
+  ami                       = data.aws_ami.ubuntu_lts.id
+  instance_type             = var.instance_type
+  subnet_id                 = aws_subnet.stal_subnet.id
+  vpc_security_group_ids    = [aws_security_group.stal_web_sg.id]
   
   associate_public_ip_address = true
 
-  tags          = {
+  tags                      = {
       Name = "stal_${var.project_name}"
   }
 }
@@ -45,4 +46,45 @@ resource "aws_subnet" "stal_subnet" {
   vpc_id = aws_vpc.stal_vpc.id
   cidr_block = "10.0.1.0/24"
   # availability_zone = "us-west-1a"
+}
+
+resource "aws_security_group" "stal_web_sg" {
+  name        = "stal_web_sg"
+  description = "Default web: 22, 80, 443"
+  vpc_id      = aws_vpc.stal_vpc.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    description = "HTTPS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "stal_web_sg"
+  }
 }
